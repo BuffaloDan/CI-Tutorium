2ea3f1bc416776de3fe446aafbc5c150e64cdc38
Daniel Kalt <BuffaloDan@indiplex.de>
2017-05-26 08:07:49 +0200

    Update 3 :D

--- Blatt3/BuildAndRun.bat
+++ Blatt3/BuildAndRun.bat
@@ -3,7 +3,7 @@ MKDIR bin
 cd ..\CI-Library
 CALL build.bat
 cd ..\Blatt3
-IF NOT EXIST MKDIR lib
+IF NOT EXIST lib MKDIR lib
 COPY /Y ..\CI-Library\target\CI-Library.jar lib\CI-Library.jar
 javac -d bin src\de\buffalodan\ci\network\*.java -cp lib\CI-Library.jar
 java -cp "lib\CI-Library.jar;bin" de.buffalodan.ci.network.Main
