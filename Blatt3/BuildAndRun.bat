efb69fdb83d6db9eb3285bdaa5c47963ed2acc6b
Daniel Kalt <BuffaloDan@indiplex.de>
2017-05-26 08:06:43 +0200

    Update BuildScirpts

--- Blatt3/BuildAndRun.bat
+++ Blatt3/BuildAndRun.bat
@@ -3,6 +3,7 @@ MKDIR bin
 cd ..\CI-Library
 CALL build.bat
 cd ..\Blatt3
-COPY ..\CI-Library\target\CI-Library.jar lib\CI-Library.jar
+IF NOT EXIST MKDIR lib
+COPY /Y ..\CI-Library\target\CI-Library.jar lib\CI-Library.jar
 javac -d bin src\de\buffalodan\ci\network\*.java -cp lib\CI-Library.jar
 java -cp "lib\CI-Library.jar;bin" de.buffalodan.ci.network.Main
--- Blatt3/BuildAndRun.sh
+++ Blatt3/BuildAndRun.sh
@@ -3,6 +3,6 @@ mkdir bin
 cd ../CI-Library
 source build.sh
 cd ../Blatt3
-COPY ../CI-Library/target/CI-Library.jar lib/CI-Library.jar
+cp ../CI-Library/target/CI-Library.jar lib/CI-Library.jar
 javac -d bin src/de/buffalodan/ci/network/*.java -cp lib/CI-Library.jar
 java -cp "lib/CI-Library.jar;bin" de.buffalodan.ci.network.Main
